ae6cb1081474ec29d2832f9f13e82cc212cb9f67
Kamila <kamila@macbook-pro.attlocal.net>
2020-02-20 20:56:22 -0800

    fixing bug with the style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./Button.css";
 
 class Button extends React.Component {
-  isOperator = val => {
-    return !isNaN(val) || val === "." || val === "=" || val === "CE";
+  isNotOperator = val => {
+    return !isNaN(val) || val === "." || val === "=";
   };
 
   render() {
     return (
       <div
         className={`button  ${
-          this.isOperator(this.props.children) ? " " : "operator"
+          this.isNotOperator(this.props.children) ? " " : "operator"
         }`}
         onClick={() => this.props.handleClick(this.props.children)}
       >
